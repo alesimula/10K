acc4576c0593aeb6ef59ab84d4c85a60084d0a42
Oliver <mr.fiber@yahoo.com>
2012-10-28 16:05:36 -0700

    Getting config setup. Next up: dimension.
    Mind the weird test code. It's just there so I don't forget how to do
    it.

--- trunk/src/mod10k/mod10k.java
+++ trunk/src/mod10k/mod10k.java
@@ -1,16 +1,28 @@
 package mod10k;
 
 import net.minecraft.src.*;
+import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "mod10k", name = "10K", version = "V0.0A")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
 
-public class mod10k
+public class Mod10K
 {
+	@PreInit
+    public void preInit(FMLPreInitializationEvent event)
+	{
+		Configuration configuration = new Configuration(event.getSuggestedConfigurationFile());
+		configuration.load();
+		int testID = configuration.getBlock("Test", 230).getInt();
+		configuration.save();
+	}
+	
 	@Init
 	public void load(FMLInitializationEvent event)
 	{
